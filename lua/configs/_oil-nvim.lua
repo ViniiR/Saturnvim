733b6b1fa60f3bcff77243ef6e79fa6f2a94df81
ViniiR <cvrm2004@gmail.com>
2025-05-04 21:00:41 -0300

    misc: add explicit defaults to oil

--- lua/configs/_oil-nvim.lua
+++ lua/configs/_oil-nvim.lua
@@ -2,6 +2,9 @@ require("oil").setup({
     view_options = {
         show_hidden = true,
     },
+    default_file_explorer = true,
+    -- uncomment to disable (unused) keymaps
+    -- use_default_keymaps = false,
 })
 
 require("mappings.setup._oil")()
