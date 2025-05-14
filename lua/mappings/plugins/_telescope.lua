8495177405e75cac065cf894c54f41687c0d9211
ViniiR <cvrm2004@gmail.com>
2025-05-13 22:13:13 -0300

    fix: add telescope actions instead of nvim_input

--- lua/mappings/plugins/_telescope.lua
+++ lua/mappings/plugins/_telescope.lua
@@ -12,8 +12,8 @@ return {
     i = {
         [keys.control_backspace] = function() vim.api.nvim_input("<C-W>") end,
         [keys.control(keys.delete)] = function() vim.api.nvim_input("<C-o>dw") end,
-        [keys.tab] = function() vim.api.nvim_input("<C-n>") end,
-        [keys.shift_tab] = function() vim.api.nvim_input("<C-p>") end,
+        [keys.tab] = telescope_actions.move_selection_next,
+        [keys.shift_tab] = telescope_actions.move_selection_previous,
         [keys.escape] = telescope_actions.close,
     },
 }
