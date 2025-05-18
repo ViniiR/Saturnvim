3dfc2d3a581433ca58956e67933dcf797cedb042
ViniiR <cvrm2004@gmail.com>
2025-05-17 21:47:47 -0300

    fix: vastly improve nvim-lspconfig config file

--- lua/configs/_nvim-lspconfig.lua
+++ lua/configs/_nvim-lspconfig.lua
@@ -6,105 +6,151 @@ local x = vim.diagnostic.severity
 --- INFO: updated in autocmds.lua
 vim.g.current_attached_lsp = "No LSP"
 
-local config = {}
-
 --- INFO: useful information
 -- disable semantic tokens completely
 -- vim.highlight.priorities.semantic_tokens = 0
 
--- WARNING: rust_analyzer is managed externally by rustaceanvim do not include it
-local lsp_list = {
-    "lua_ls",
-    "ts_ls",
-    "eslint",
-    "emmet_language_server",
-    "html",
-    "cssls",
-    "tailwindcss",
-    "prismals",
-    "jsonls",
-    "yamlls",
-    "bashls",
-    "clangd",
-    "neocmake",
-    "nil_ls",
-    "nixd",
-    -- "cssmodules_ls", -- not found
-    -- "css-variables-lsp", -- not found
-    -- "somesass_ls", -- never used, not found
-}
-
-vim.diagnostic.config({
-    virtual_text = enable_native_virtual_text and {
-        prefix = VIRTUAL_TEXT_PREFIX,
-        virt_text_hide = false,
-    } or false,
-    signs = {
-        text = {
-            [x.ERROR] = LSP_SYMBOLS.ERROR,
-            [x.WARN] = LSP_SYMBOLS.WARN,
-            [x.INFO] = LSP_SYMBOLS.INFO,
-            [x.HINT] = LSP_SYMBOLS.HINT,
+local config = {
+    hover_window_config = {
+        title = string.format(" %s Info ", INFO_ICON),
+        border = BORDER_KIND,
+        max_width = nil, -- TODO
+        max_height = nil,
+    },
+    diagnostic_config = {
+        virtual_text = enable_native_virtual_text and {
+            prefix = VIRTUAL_TEXT_PREFIX,
+            virt_text_hide = false,
+        } or false,
+        signs = {
+            text = {
+                [x.ERROR] = LSP_SYMBOLS.ERROR,
+                [x.WARN] = LSP_SYMBOLS.WARN,
+                [x.INFO] = LSP_SYMBOLS.INFO,
+                [x.HINT] = LSP_SYMBOLS.HINT,
+            },
+        },
+        underline = true,
+        float = {
+            title = string.format(" %s Diagnostics ", DIAGNOSTICS_ICON),
+            border = BORDER_KIND,
+            header = " ",
+            prefix = function(_, i, _)
+                return " " .. i .. ": "
+            end,
+            suffix = function(diagnostic, i, total)
+                local str = " "
+                if diagnostic.code ~= nil then
+                    str = " [" .. diagnostic.code .. "] "
+                end
+                if i == total then
+                    str = str .. "\n   "
+                end
+                return str
+            end,
+            focusable = true,
+            severity_sort = true,
+        },
+        severity_sort = true,
+        update_in_insert = false,
+    },
+    publish_diagnostics_config = {
+        virtual_text = enable_native_virtual_text and {
+            prefix = function(diagnostic)
+                local vim_diagnostic = vim.diagnostic.severity
+                local severity = diagnostic.severity
+                if severity == vim_diagnostic.ERROR then
+                    return " " .. LSP_SYMBOLS.ERROR
+                elseif severity == vim_diagnostic.WARN then
+                    return " " .. LSP_SYMBOLS.WARN
+                elseif severity == vim_diagnostic.INFO then
+                    return " " .. LSP_SYMBOLS.INFO
+                elseif severity == vim_diagnostic.HINT then
+                    return " " .. LSP_SYMBOLS.HINT
+                else
+                    return VIRTUAL_TEXT_PREFIX
+                end
+            end,
+        } or false,
+    },
+    signature_help_config = {
+        border = BORDER_KIND,
+        -- width = 80,
+    },
+    sign_list = {
+        ["DiagnosticSignError"] = {
+            text = LSP_SYMBOLS.ERROR,
+            texthl = "DiagnosticSignError",
+        },
+        ["DiagnosticSignWarn"] = {
+            text = LSP_SYMBOLS.WARN,
+            texthl = "DiagnosticSignWarn",
+        },
+        ["DiagnosticSignInfo"] = {
+            text = LSP_SYMBOLS.INFO,
+            texthl = "DiagnosticSignInfo",
+        },
+        ["DiagnosticSignHint"] = {
+            text = LSP_SYMBOLS.HINT,
+            texthl = "DiagnosticSignHint",
         },
     },
-    underline = true,
-    float = {
-        header = "",
-        title = " 󰺄 Diagnostics ",
-        border = BORDER_KIND or "single",
-        focusable = true,
+    -- WARNING: rust_analyzer is managed externally by rustaceanvim do not include it
+    lsp_list = {
+        "lua_ls",
+        "ts_ls",
+        "eslint",
+        "emmet_language_server",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "prismals",
+        "jsonls",
+        "yamlls",
+        "bashls",
+        "clangd",
+        "neocmake",
+        -- "nil_ls",
+        "nixd",
+        -- not found in nix
+        -- "cssmodules_ls", "css-variables-lsp"
+        -- not found in nix (but also never used)
+        -- "somesass_ls"
     },
-})
+}
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = BORDER_KIND or "single",
-    max_width = nil, -- TODO
-    max_height = nil,
-    title = " " .. INFO_ICON .. " Info ",
-})
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = enable_native_virtual_text and {
-        prefix = function(diagnostic)
-            local vim_diagnostic = vim.diagnostic.severity
-            local severity = diagnostic.severity
-            if severity == vim_diagnostic.ERROR then
-                return " " .. LSP_SYMBOLS.ERROR
-            elseif severity == vim_diagnostic.WARN then
-                return " " .. LSP_SYMBOLS.WARN
-            elseif severity == vim_diagnostic.INFO then
-                return " " .. LSP_SYMBOLS.INFO
-            elseif severity == vim_diagnostic.HINT then
-                return " " .. LSP_SYMBOLS.HINT
-            else
-                return VIRTUAL_TEXT_PREFIX
-            end
-        end,
-    } or false,
-})
+for key, sign in pairs(config.sign_list) do
+    vim.fn.sign_define(key, sign)
+end
+
+vim.diagnostic.config(config.diagnostic_config)
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, config.hover_window_config)
+
+vim.lsp.handlers["textDocument/signatureHelp"] =
+    vim.lsp.with(vim.lsp.handlers.signature_help, config.signature_help_config)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] =
+    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, config.publish_diagnostics_config)
 
 local mappings_setup = require("mappings.setup._lspconfig")
 
-config.on_attach = function(client, bufnr)
-    -- INFO
-    -- prevents nixd from showing hover diagnostics in favor of nil_ls's better hover
-    -- ensure nil_ls enabled
-    if client.name == "nixd" then
-        client.server_capabilities.hoverProvider = false
-    end
+config["on_attach"] = function(_, bufnr)
     mappings_setup(bufnr)
 
     vim.lsp.inlay_hint.enable(true)
     vim.diagnostic.config({ virtual_text = enable_native_virtual_text and true or false })
 end
 
-config.on_init = function(client, _)
+config["on_init"] = function(client, _)
     if client.supports_method("textDocument/semanticTokens") then
         client.server_capabilities.semanticTokensProvider = nil
     end
 end
 
-config.capabilities = vim.lsp.protocol.make_client_capabilities()
-config.capabilities.textDocument.completion.completionItem = {
+config["capabilities"] = vim.lsp.protocol.make_client_capabilities()
+
+config["capabilities"].textDocument.completion.completionItem = {
     documentationFormat = { "markdown", "plaintext" },
     snippetSupport = true,
     preselectSupport = true,
@@ -126,7 +172,7 @@ config.capabilities.textDocument.completion.completionItem = {
     },
 }
 
-for _, lsp_name in pairs(lsp_list) do
+for _, lsp_name in ipairs(config.lsp_list) do
     local lspconf = {
         on_init = config.on_init,
         on_attach = config.on_attach,
@@ -178,23 +224,3 @@ for _, lsp_name in pairs(lsp_list) do
 end
 
 return config
-
--- TODO: uncomment if broken
--- local sign = vim.fn.sign_define
---
--- sign("DiagnosticSignError", {
---     text = LSP_SYMBOLS.ERROR,
---     texthl = "DiagnosticSignError",
--- })
--- sign("DiagnosticSignWarn", {
---     text = LSP_SYMBOLS.WARN,
---     texthl = "DiagnosticSignWarn",
--- })
--- sign("DiagnosticSignInfo", {
---     text = LSP_SYMBOLS.INFO,
---     texthl = "DiagnosticSignInfo",
--- })
--- sign("DiagnosticSignHint", {
---     text = LSP_SYMBOLS.HINT,
---     texthl = "DiagnosticSignHint",
--- })
